92ea4d768daad8eb95aca974589b59fa6a71e9c0
JakubLatawiec <106024697+JakubLatawiec@users.noreply.github.com>
2024-12-29 20:30:26 +0100

    fix: next config

--- next-app/next.config.ts
+++ next-app/next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 import path from "path";
 import dotenv from "dotenv"
 
-dotenv.config({ path: path.resolve(__dirname, "../.env") });
+//For production
+//dotenv.config({ path: path.resolve(__dirname, "../.env") });
 
 const nextConfig: NextConfig = {
   env: {
