1cbc35448cef247d807e5606e7cce790beeb310b
JakubLatawiec <106024697+JakubLatawiec@users.noreply.github.com>
2024-12-29 22:53:32 +0100

    fix: styles

--- next-app/components/ui/navbar/change-location-button.tsx
+++ next-app/components/ui/navbar/change-location-button.tsx
@@ -10,7 +10,7 @@ interface ChangeLocationButtonProps {
 const ChangeLocationButton: React.FC<ChangeLocationButtonProps> = ({handleClick}) => {
     return (
         <div className="flex flex-row justify-end items-center">
-            <p className="sm:block hidden font-chakra font-medium dark:font-light">Change location:</p>
+            <p className="sm:block hidden font-chakra font-medium dark:font-light mr-[16px]">Change location:</p>
             <IconButton className="!sm:ml-[8px]" onClick={handleClick}>
                 <MapIcon />
             </IconButton>
