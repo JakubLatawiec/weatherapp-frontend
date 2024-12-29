f47036fa60abdc5683d72b89827526e1956dbcfc
JakubLatawiec <106024697+JakubLatawiec@users.noreply.github.com>
2024-12-29 20:18:51 +0100

    fix: env variables

--- next-app/next.config.ts
+++ next-app/next.config.ts
@@ -6,8 +6,8 @@ dotenv.config({ path: path.resolve(__dirname, "../.env") });
 
 const nextConfig: NextConfig = {
   env: {
-    API_URL: process.env.API_URL || "https://localhost",
-    API_PORT: process.env.API_PORT || "7064",
+    API_URL: process.env.API_URL ?? "https://localhost",
+    API_PORT: process.env.API_PORT ?? "7064",
   }
 };
 
