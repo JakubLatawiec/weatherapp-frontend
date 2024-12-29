6bd90fad58b6a35e3369f40a45ee2f7a1daab634
JakubLatawiec <106024697+JakubLatawiec@users.noreply.github.com>
2024-12-29 22:46:47 +0100

    fix: tailwind render

--- next-app/tailwind.config.ts
+++ next-app/tailwind.config.ts
@@ -1,6 +1,9 @@
 import type { Config } from "tailwindcss";
 
 export default {
+  corePlugins: {
+    preflight: false
+  },
   darkMode: "class",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
